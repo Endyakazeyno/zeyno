7cd543bd6d79c74e0a2f5f05b5c33ed92dcd6975
Endyakazeyno <endy89163@gmail.com>
2026-05-18 08:36:40 +0200

    Create funzione-anti-privato.js

---
+++ plugins/funzione-anti-privato.js
@@ -0,0 +1,15 @@
+// Plugin AntiPrivato by endy
+
+export async function before(m, { isOwner, isRowner, isModerator }) {
+  if (m.fromMe) return true
+  if (m.isGroup) return false
+  if (!m.message) return true
+
+  const bot = global.db.data.settings[this.user.jid] || {}
+
+  if (bot.antiprivato && !isOwner && !isRowner && !isModerator) {
+    await this.updateBlockStatus(m.chat, 'block')
+  }
+
+  return false
+}
